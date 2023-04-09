1c5d9c03cc6028f89dc6852458faa1fcc1c6ab73
Vaibhav440 <128299280+Vaibhav440@users.noreply.github.com>
2023-04-09 17:49:52 +0530

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MaevellousFirst'`, () => {
+  it(`should have as title 'First'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MaevellousFirst');
+    expect(app.title).toEqual('First');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MaevellousFirst app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('First app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    FirstCompComponent
   ],
   imports: [
     BrowserModule
